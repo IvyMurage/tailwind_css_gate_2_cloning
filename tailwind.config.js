bb764b647103a8edda8aabeed8538e05f93c7665
Murage-Ivy <ivymurage2000@gmail.com>
2024-05-17 08:59:52 +0200

    Add fonts and images assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,23 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "primary-100": "#F0F4FF",
+        "primary-200": "#D9E2FF",
+        "primary-300": "#A6C1FF",
+        "secondary-100": "#F0F4FF",
+        "secondary-200": "#D9E2FF",
+      },
       borderWidth: {
         3: "3px",
         1: "1px",
       },
     },
     fontFamily: {
-      sans: ["Inter", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
     },
     backgroundImage: {
-      "hero-pattern":
-        "url('https://source.unsplash.com/1600x900/?nature')",
+      "hero-pattern": "url('/images/hero-pattern.svg')",
     },
   },
   plugins: [],
